e92e550e2afec8eb9be30d9177a6974dfcded5dd
AliDurmus21 <148628197+AliDurmus21@users.noreply.github.com>
2023-10-21 15:53:57 +0300

    Add files via upload

---
+++ Java101/src/NotOrtalamasiHesaplama.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class NotOrtalamasiHesaplama {
+    public static void main(String[] args) {
+
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuzu Giriniz : ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuzu Giriniz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuzu Giriniz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuzu Giriniz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuzu Giriniz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuzu Giriniz : ");
+        muzik = inp.nextInt();
+
+        int toplam = mat + fizik + kimya + tarih + turkce + muzik;
+        double sonuc = toplam/6.0;
+        System.out.println("Not Ortalamanız : " + sonuc );
+
+        boolean gecti = sonuc >= 60;
+        String str = gecti ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(" : " + str);
+    }
+}
